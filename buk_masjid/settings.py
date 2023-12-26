ab7d9774fccc3ae01888bd99553b5cc643c2d19f
gittyhub82 <154790238+gittyhub82@users.noreply.github.com>
2023-12-26 16:37:13 +0100

    Update settings.py

--- buk_masjid/settings.py
+++ buk_masjid/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ""
+SECRET_KEY = "django-insecure-fig66d71ky4&0y6bbfdb%(8-$6h8_j-8vxp)p8_xxxcj(9q==a"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
